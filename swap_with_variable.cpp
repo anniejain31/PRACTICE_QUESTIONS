d7edb3870c9a7121189214cd3dd37d219faf9e11
Annie Jain <87299172+anniejain31@users.noreply.github.com>
2023-02-07 14:33:47 +0530

    Add files via upload

---
+++ swap_with_variable.cpp
@@ -0,0 +1,18 @@
+// C++ program to swap two
+// numbers using 3rd variable
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a = 2, b = 3;
+	
+	cout << "Before swapping a = " <<
+			a << " , b = " << b << endl;
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << "After swapping a = " << a <<
+			" , b = " << b << endl;
+	return 0;
+}
