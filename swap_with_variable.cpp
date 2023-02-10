932a6b2210488a14ff8d62bf2326fdf3358735e3
Annie Jain <87299172+anniejain31@users.noreply.github.com>
2023-02-10 11:43:43 +0530

    Update swap_with_variable.cpp

--- swap_with_variable.cpp
+++ swap_with_variable.cpp
@@ -1,18 +1,21 @@
-// C++ program to swap two
-// numbers using 3rd variable
-#include<bits/stdc++.h>
+//SWAPPING WITH THIRD VARIABLE
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-	int a = 2, b = 3;
-	
-	cout << "Before swapping a = " <<
-			a << " , b = " << b << endl;
-	int temp;
-	temp = a;
-	a = b;
-	b = temp;
-	cout << "After swapping a = " << a <<
-			" , b = " << b << endl;
-	return 0;
+    int num1, num2;
+    cout<<"Enter Num 1:"<<endl;
+    cin>>num1;
+    cout<<"Enter Num 2:"<<endl;
+    cin>>num2;
+    int temp=num1+num2;
+    num2=temp-num2;
+    num1=temp-num1;
+    cout<<"Number 1: "<<num1<<endl;
+    cout<<"Number 2: "<<num2<<endl;
+
+    return 0;
 }
+
